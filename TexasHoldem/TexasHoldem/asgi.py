942b24b29d6706aac85bb71e08b720db6a0d634a
katie <547046456@qq.com>
2024-07-08 22:22:09 +0800

    django framework

---
+++ TexasHoldem/TexasHoldem/asgi.py
@@ -0,0 +1,29 @@
+"""
+ASGI config for TexasHoldem project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+from .routing import websocket_urlpatterns
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TexasHoldem.settings')
+
+application = ProtocoTypeRouter({
+    "http" : get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    )
+})
